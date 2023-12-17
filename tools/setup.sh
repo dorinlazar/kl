cff895de110f3755fc8b444908828cf93138ef7a
Dorin Lazăr <dorin.lazar@gmail.com>
2023-12-17 12:55:23 +0200

    chore: setup

--- tools/setup.sh
+++ tools/setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+sudo dnf groupinstall -y "Development Tools"
 sudo dnf install -y clang-tools-extra clang gcc-g++ ninja-build fmt-devel gtest-devel gmock-devel openssl-devel \
-                    cppcheck valgrind lcov python3-devel pip
+                    cppcheck valgrind lcov python3-devel pip cmake
 pip install CodeChecker cmake-format
