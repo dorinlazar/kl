86c60c9b9a44ac7e44deb360791cc949586816c8
Dorin Lazăr <dorin.lazar@gmail.com>
2023-12-24 06:49:56 +0200

    chore: ignore mismatch errors in lcov for some reason I don't get

--- tools/run-coverage.sh
+++ tools/run-coverage.sh
@@ -8,9 +8,9 @@ cmake --preset coverage
 cmake --build --preset coverage
 
 mkdir "${BUILD_DIR}/coverage"
-lcov --capture --no-external --initial --directory . -o "${BUILD_DIR}/coverage/zero-coverage.info"
+lcov --ignore-errors mismatch --capture --no-external --initial --directory . -o "${BUILD_DIR}/coverage/zero-coverage.info"
 "${BUILD_DIR}/test/kl/kltests"
-lcov --capture --no-external --directory .  --output-file "${BUILD_DIR}/coverage/kltests-coverage.info" 
-lcov --add-tracefile "${BUILD_DIR}/coverage/zero-coverage.info" --add-tracefile "${BUILD_DIR}/coverage/kltests-coverage.info" --output-file "${BUILD_DIR}/coverage/full-coverage.info"
-lcov --remove "${BUILD_DIR}/coverage/full-coverage.info" "${PWD}/test/*" --output-file "${BUILD_DIR}/coverage/coverage.info"
+lcov --ignore-errors mismatch --capture --no-external --directory .  --output-file "${BUILD_DIR}/coverage/kltests-coverage.info" 
+lcov --ignore-errors mismatch --add-tracefile "${BUILD_DIR}/coverage/zero-coverage.info" --add-tracefile "${BUILD_DIR}/coverage/kltests-coverage.info" --output-file "${BUILD_DIR}/coverage/full-coverage.info"
+lcov --ignore-errors mismatch --remove "${BUILD_DIR}/coverage/full-coverage.info" "${PWD}/test/*" --output-file "${BUILD_DIR}/coverage/coverage.info"
 genhtml "${BUILD_DIR}/coverage/coverage.info" --output-directory "${BUILD_DIR}/coverage/"
