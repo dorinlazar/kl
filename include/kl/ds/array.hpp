7f25495e442b5e70cfd6b1023d4ac4fd6d6b90b0
Dorin Lazăr <dorin.lazar@gmail.com>
2023-12-24 23:16:37 +0200

    mi-e somn

--- include/kl/ds/array.hpp
+++ include/kl/ds/array.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <kl/inttypes.hpp>
 #include <kl/ds/tags.hpp>
+#include <kl/except.hpp>
 
 namespace kl {
 
@@ -10,10 +11,27 @@ class Array {
   TSize m_size;
   TSize m_reserved;
 
+  TSize get_index(TSize index) const {
+    if (index < 0) {
+      index += m_size;
+    }
+    if (index >= m_size || index < 0) [[unlikely]] {
+      throw RuntimeError("Out of range");
+    }
+    return index;
+  }
+
+  void release_data() {
+    for (TSize i = 0; i < m_size; ++i) {
+      m_data[i].~T();
+    }
+    ::operator delete(m_data);
+  }
+
 public:
   constexpr Array() : m_data(nullptr), m_size(0), m_reserved(0) {}
   template <typename... Args>
-  constexpr Array(TagBuild, TSize size, Args...&& args)
+  constexpr Array(TagBuild, TSize size, Args&&... args)
       : m_data(static_cast<T*>(::operator new(size * sizeof(T)))), m_size(size), m_reserved(size) {
     for (TSize i = 0; i < m_size; ++i) {
       new (m_data + i) T(std::forward<Args>(args)...);
@@ -28,25 +46,14 @@ public:
     for (TSize i = 0; i < m_size; ++i) {
       m_data[i].~T();
     }
-    delete static_cast<void*>(m_data);
+    ::operator delete(m_data);
     m_data = nullptr;
     m_size = 0;
     m_reserved = 0;
   }
 
-  template <class Self>
-  constexpr auto&& operator[](this Self&& self, TSize index) {
-    if (self.m_ptr == nullptr) [[unlikely]] {
-      throw RuntimeError("Null reference");
-    }
-    if (index < 0) {
-      index += self.m_size;
-    }
-    if (index >= self.m_size || index < 0) [[unlikely]] {
-      throw RuntimeError("Out of range");
-    }
-    return self.m_ptr[index];
-  }
+  constexpr const auto& operator[](TSize index) const { return m_data[get_index(index)]; }
+  constexpr auto& operator[](TSize index) { return m_data[get_index(index)]; }
 
   constexpr TSize size() const { return m_size; }
   constexpr TSize reserved() const { return m_reserved; }
@@ -57,9 +64,8 @@ public:
       auto new_data = static_cast<T*>(::operator new(size * sizeof(T)));
       for (TSize i = 0; i < m_size; ++i) {
         new (new_data + i) T(std::move(m_data[i]));
-        m_data[i].~T();
       }
-      delete static_cast<void*> m_data;
+      release_data();
       m_data = new_data;
       m_reserved = size;
     }
