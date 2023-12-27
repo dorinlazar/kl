cb2ff2950c3536b61a93c9f4e4f18e8a39086a22
Dorin Lazăr <dorin.lazar@gmail.com>
2023-12-27 09:09:30 +0200

    stuff

--- tools/run-coverage.sh
+++ tools/run-coverage.sh
@@ -9,7 +9,7 @@ cmake --build --preset coverage
 
 mkdir "${BUILD_DIR}/coverage"
 lcov --ignore-errors mismatch --capture --no-external --initial --directory . -o "${BUILD_DIR}/coverage/zero-coverage.info"
-"${BUILD_DIR}/test/kl/kltests"
+"${BUILD_DIR}/test/kltests"
 lcov --ignore-errors mismatch --capture --no-external --directory .  --output-file "${BUILD_DIR}/coverage/kltests-coverage.info" 
 lcov --ignore-errors mismatch --add-tracefile "${BUILD_DIR}/coverage/zero-coverage.info" --add-tracefile "${BUILD_DIR}/coverage/kltests-coverage.info" --output-file "${BUILD_DIR}/coverage/full-coverage.info"
 lcov --ignore-errors mismatch --remove "${BUILD_DIR}/coverage/full-coverage.info" "${PWD}/test/*" --output-file "${BUILD_DIR}/coverage/coverage.info"
