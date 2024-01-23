b7d5cf55967a91bb0d68974c01ba63e45d83e739
Dorin Lazăr <dorin.lazar@gmail.com>
2024-01-23 22:38:58 +0200

    mi-e somn

---
+++ include/kl/ds/dict.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "pair.hpp"
+#include "array.hpp"
+
+namespace kl {
+template <typename Key, typename Value>
+class Dict {
+public:
+};
+
+} // namespace kl
---
+++ include/kl/ds/pair.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace kl {
+template <typename T1, typename T2>
+class Pair {
+public:
+  T1 first;
+  T2 second;
+};
+} // namespace kl
